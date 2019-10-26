3e8d5bc48c4153120626816d6bcf4d1353ac9945
Barboos <33928663+Acraiu@users.noreply.github.com>
2019-10-27 02:37:40 +0300

    Add project

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    //Configuration parameters
+    [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush, yPush;
+    [SerializeField] AudioClip []ballSounds;
+
+    //Cache components
+    AudioSource myAudioSource;
+
+    bool hasStarted;
+    Vector2 paddleToBallPosition;
+
+	// Use this for initialization
+	void Start () {
+        paddleToBallPosition = transform.position - paddle1.transform.position;
+        hasStarted = false;
+        myAudioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (hasStarted)
+        {
+            AudioClip ballSound = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+            myAudioSource.PlayOneShot(ballSound);
+        }
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePosition = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePosition + paddleToBallPosition;
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    //Configuration paramaters
+    [SerializeField] float minX, maxX;
+
+    [SerializeField] float screenWidthInUnits;
+    private float mousePosition;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mousePosition = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Vector2 paddlePosition = new Vector2(transform.position.x, transform.position.y);
+        paddlePosition.x = Mathf.Clamp(mousePosition, minX, maxX);
+        transform.position = paddlePosition;
+    }
+}
